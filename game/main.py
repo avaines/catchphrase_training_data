4dc066a60ce404ae00773ef97f90d4a61068c159
avaines <aiden@vaines.org>
2023-08-21 14:47:09 +0100

    minor style updates

--- game/main.py
+++ game/main.py
@@ -9,41 +9,48 @@ app = Flask(__name__)
 
 class gameState:
     def __init__(self):
-        catchphrases_dir = "./game/static/catchphrases/"
-        catchphrases = []
-        player_one_score = 0
-        player_two_score = 0
-        current_catchphrase_value = 1000
+        self.clicked_boxes = []
+        self.catchphrase_filename = "./static/" + "base.png"
+        self.catchphrases_dir = "./game/static/catchphrases/"
+        self.catchphrases = []
+        self.player_one_score = 0
+        self.player_two_score = 0
+        self.current_catchphrase_value = 0
 
-        for file in os.listdir(catchphrases_dir):
+        for file in os.listdir(self.catchphrases_dir):
             if file.endswith('.png'):
-                catchphrases.append(file)
-        catchphrase_num = random.randint(0, len(catchphrases)-1)
-        current_image = catchphrases_dir + catchphrases[catchphrase_num]
+                self.catchphrases.append(file)
 
+    def selectCatchphrase(self):
+        catchphrase_num = random.randint(0, len(self.catchphrases)-1)
+        self.current_image = self.catchphrases_dir + self.catchphrases[catchphrase_num]
+        del self.catchphrases[catchphrase_num]
+        self.current_catchphrase_value = 1000
+        self.clicked_boxes = [0,1,2,3,4,5,6,7,8]
 
 @app.route('/')
 def index():
-    return render_template("index.html", catchphrase_image=catchphrase_filename)
+    return render_template("index.html", catchphrase_image=Game.catchphrase_filename, p1_score=Game.player_one_score, p2_score=Game.player_two_score, catchprase_value=Game.current_catchphrase_value)
 
 
 @app.route('/box_clicked/<box_number>')
 def box_clicked(box_number):
     try:
-        clicked_boxes.remove(int(box_number))
+        Game.clicked_boxes.remove(int(box_number))
+        Game.current_catchphrase_value -= 100
     except ValueError:
         pass
 
-    image = cv2.imread(current_image)
+    image = cv2.imread(Game.current_image)
     sections = divide_image(image)
 
-    overlaid_image = apply_overlay_cards(image, clicked_boxes, "./game/static/catchphrase-bjss-block.png", sections)
+    overlaid_image = apply_overlay_cards(image, Game.clicked_boxes, "./game/static/catchphrase-bjss-block.png", sections)
 
     overlaid_image_pil = Image.fromarray(overlaid_image)
     overlaid_image_pil.save("./game/static/game-image.png")
-    catchphrase_filename = "./static/game-image.png"
+    catchphrase_filename = "/static/game-image.png"
 
-    return render_template("index.html", catchphrase_image="../" + catchphrase_filename, p1_score=player_one_score, p2_score=player_two_score, catchprase_value=current_catchphrase_value)
+    return render_template("index.html", catchphrase_image=catchphrase_filename, p1_score=Game.player_one_score, p2_score=Game.player_two_score, catchprase_value=Game.current_catchphrase_value)
 
 
 def divide_image(image):
@@ -75,9 +82,8 @@ def apply_overlay_cards(image, sections_to_replace, replacement_image_path, sect
 
 
 if __name__ == '__main__':
-    clicked_boxes = [0,1,2,3,4,5,6,7,8]
-    catchphrase_filename = "./static/" + "base.png"
-    
+    Game = gameState()
+    Game.selectCatchphrase()
 
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
 
